26694f0afbc7442af8b23f338c33d2487acb2769
Alessandro Molari <molari.alessandro@gmail.com>
2018-08-21 20:33:34 +0200

    Remove metalsmith-if and use standalone conditions

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const log = require("debug")("cyberloop");
 // Plugins
 const debug = require("metalsmith-debug");
 const watch = require("metalsmith-watch");
-const msIf = require("metalsmith-if");
 const drafts = require("metalsmith-drafts");
 const layouts = require("metalsmith-layouts");
 const permalinks = require("metalsmith-permalinks");
@@ -17,7 +16,12 @@ const argv = minimist(process.argv.slice(2), {
   string: ["environment", "algolia"],
   boolean: ["watch", "deploy"],
   alias: { e: "environment", w: "watch", d: "deploy", a: "algolia" },
-  default: { environment: "development", watch: false, deploy: false }
+  default: {
+    environment: "development",
+    watch: false,
+    deploy: false,
+    algolia: null
+  }
 });
 
 const environment = argv.environment;
@@ -35,82 +39,81 @@ if (shouldDeploy && !algoliaPrivateKey) {
 log("Using environment %o", environment);
 if (watchEnabled) log("Livereload enabled, watching for file changes..");
 if (shouldDeploy) log("Deployment is enabled");
+if (algoliaPrivateKey) log("Updating Algolia Index");
 
 const metadata = Object.assign(require("./metadata"), {
   environment,
   watchEnabled
 });
 
-Metalsmith(__dirname)
-  .source("./src")
-  .destination(buildDirectory)
-  .metadata(metadata)
-  .use(drafts())
-  .use(
-    layouts({
-      directory: "./layouts",
-      default: "default.hbs"
+m = Metalsmith(__dirname);
+m.source("./src");
+m.destination(buildDirectory);
+m.metadata(metadata);
+m.use(drafts());
+m.use(
+  layouts({
+    directory: "./layouts",
+    default: "default.hbs"
+  })
+);
+m.use(
+  permalinks({
+    pattern: ":title"
+  })
+);
+if (watchEnabled) {
+  m.use(
+    watch({
+      paths: {
+        "${source}/**/*": true,
+        "layouts/**/*": "**/*",
+        "metadata/**/*": "**/*"
+      },
+      livereload: watchEnabled
     })
-  )
-  .use(
-    permalinks({
-      pattern: ":title"
+  );
+}
+if (algoliaPrivateKey) {
+  m.use(
+    algolia({
+      projectId: "RFJN6AJVVQ",
+      privateKey: algoliaPrivateKey,
+      index: "CONTENT"
     })
-  )
-  .use(
-    msIf(
-      watchEnabled,
-      watch({
-        paths: {
-          "${source}/**/*": true,
-          "layouts/**/*": "**/*",
-          "metadata/**/*": "**/*"
+  );
+}
+m.use(debug());
+m.build(err => {
+  const buildFinishTime = new Date().toISOString();
+
+  if (err) {
+    log(err);
+  } else {
+    log("Build successfully finished at %o", buildFinishTime);
+
+    if (shouldDeploy) {
+      log(
+        "Performing deployment of %o to %o",
+        buildDirectory,
+        metadata.deployment.repository
+      );
+
+      ghpages.publish(
+        buildDirectory,
+        {
+          branch: metadata.deployment.branch,
+          repo: metadata.deployment.repository,
+          message: `Website Built at ${buildFinishTime}`
         },
-        livereload: watchEnabled
-      })
-    )
-  )
-  .use(debug())
-  .use(
-    msIf(
-      algoliaPrivateKey,
-      algolia({
-        projectId: "RFJN6AJVVQ",
-        privateKey: algoliaPrivateKey,
-        index: "CONTENT"
-      })
-    )
-  )
-  .build(err => {
-    const buildFinishTime = new Date().toISOString();
-
-    if (err) {
-      log(err);
-    } else {
-      log("Build successfully finished at %o", buildFinishTime);
-
-      if (shouldDeploy) {
-        log(
-          "Performing deployment of %o to %o",
-          buildDirectory,
-          metadata.deployment.repository
-        );
-
-        ghpages.publish(
-          buildDirectory,
-          {
-            branch: metadata.deployment.branch,
-            repo: metadata.deployment.repository,
-            message: `Website Built at ${buildFinishTime}`
-          },
-          err => {
-            if (err) {
-              log(err);
-            } else {
-              log("Deployment successfully finished");
-            }
+        err => {
+          if (err) {
+            log(err);
+          } else {
+            log("Deployment successfully finished");
           }
-        );
-      }
+        }
+      );
     }
-  });
+  }
+});
